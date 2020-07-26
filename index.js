099c41ae8ababcdc4bb1b909a73088d36fefa6c2
Luka Maisuradze <lmaisuradz3@gmail.com>
2020-07-26 15:56:43 +0400

    Initialization

---
+++ index.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import { ApolloServer, SchemaDirectiveVisitor } from "apollo-server-express";
+import http from "http";
+
+import schema from "./schema";
+import resolvers from "./resolvers";
+import models, { sequelize } from "./models";
+
+const app = express();
+
+app.subscribe(cors());
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: { models },
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+sequelize.sync().then(async () => {
+  httpServer.listen({ port: 8000 }, () => {
+    console.log("Apollo Server on http://localhost:8000/graphql");
+  });
+});
