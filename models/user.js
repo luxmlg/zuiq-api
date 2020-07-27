6759318f0567078ada1211e62756a8918c00811e
Luka Maisuradze <lmaisuradz3@gmail.com>
2020-07-27 21:02:58 +0400

    Add the ability to sign up

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
+import bcrypt from "bcrypt";
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,13 +16,49 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
-      username: DataTypes.STRING,
-      email: DataTypes.STRING,
+      username: {
+        type: DataTypes.STRING,
+        unqiue: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [7, 42],
+        },
+      },
     },
     {
       sequelize,
       modelName: "User",
     }
   );
+  User.beforeCreate(async (user) => {
+    user.password = await user.generatePasswordHash();
+  });
+
+  User.prototype.generatePasswordHash = async function () {
+    const saltRounds = 10;
+    return await bcrypt.hash(this.password, saltRounds);
+  };
+
+  User.prototype.validatePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
   return User;
 };
--- resolvers/user.js
+++ resolvers/user.js
@@ -1,11 +1,42 @@
+import jwt from "jsonwebtoken";
+import { AuthenticationError, UserInputError } from "apollo-server";
+
+const createToken = async (user, secret, expiresIn) => {
+  const { id, email, username } = user;
+  return await jwt.sign({ id, email, username }, secret, { expiresIn });
+};
+
 export default {
   Query: {
+    me: async (parent, args, { models, me }) => {
+      if (!me) {
+        return null;
+      }
+
+      return await models.User.findByPk(me.id);
+    },
     users: async (parent, args, { models }) => {
       return await models.User.findAll();
     },
 
     user: async (parent, { id }, { models }) => {
       return await models.User.findByPk(id);
+    },
+  },
+
+  Mutation: {
+    signUp: async (
+      parent,
+      { username, email, password },
+      { models, secret }
+    ) => {
+      const user = await models.User.create({
+        username,
+        email,
+        password,
+      });
+
+      return { token: createToken(user, secret, "30m") };
     },
   },
 };
