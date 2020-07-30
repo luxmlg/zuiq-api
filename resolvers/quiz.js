cb163b887f5565768a7b6b7fbff9572a53a595af
Luka Maisuradze <lmaisuradz3@gmail.com>
2020-07-30 15:56:30 +0400

    Add the ability to create and delete quizzes

--- resolvers/quiz.js
+++ resolvers/quiz.js
@@ -1,3 +1,6 @@
+import { combineResolvers } from "graphql-resolvers";
+import { isAuthenticated, isQuizOwner } from "./authorization";
+
 export default {
   Query: {
     quizzes: async (parent, args, { models }) => {
@@ -7,5 +10,25 @@ export default {
     quiz: async (parent, { id }, { models }) => {
       return await models.Quiz.findByPk(id);
     },
+  },
+  Mutation: {
+    createQuiz: combineResolvers(
+      isAuthenticated,
+      async (parent, { text }, { models, me }) => {
+        const quiz = await models.Quiz.create({
+          text,
+          UserId: me.id,
+        });
+
+        return quiz;
+      }
+    ),
+    deleteQuiz: combineResolvers(
+      isAuthenticated,
+      isQuizOwner,
+      async (parent, { id }, { models }) => {
+        return await models.Quiz.destroy({ where: { id } });
+      }
+    ),
   },
 };
--- schema/quiz.js
+++ schema/quiz.js
@@ -6,8 +6,14 @@ export default gql`
     quiz(id: ID!): Quiz!
   }
 
+  extend type Mutation {
+    createQuiz(name: String!, text: String!): Quiz!
+    deleteQuiz(id: ID!): Boolean!
+  }
+
   type Quiz {
     id: ID!
+    name: String!
     text: String!
     createdAt: Date!
     user: User!
