d8c1bdbbe4ee2c635d2790438b17745b068594dd
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-03-20 18:41:24 +0400

    Fix unable to fetch data while p-token is invalid

--- index.js
+++ index.js
@@ -14,60 +14,61 @@ const app = express();
 app.subscribe(cors());
 
 const getMe = async (req) => {
-  const token = req.headers["x-token"];
+	const token = req.headers["x-token"];
 
-  if (token) {
-    try {
-      return await jwt.verify(token, process.env.SECRET);
-    } catch (e) {
-      throw new AuthenticationError("Your session expired. Sign in again.");
-    }
-  }
+	if (token) {
+		try {
+			return await jwt.verify(token, process.env.SECRET);
+		} catch (e) {
+			throw new AuthenticationError("Your session expired. Sign in again.");
+		}
+	}
 };
 
 const getParticipantMe = async (req) => {
-  const token = req.headers["ptoken"];
+	const token = req.headers["ptoken"];
 
-  if (token) {
-    try {
-      console.log(token);
-      return await jwt.verify(token, process.env.SECRET);
-    } catch (e) {
-      throw new AuthenticationError("Meeting ended or Invalid Token.");
-    }
-  }
+	if (token) {
+		try {
+			console.log(token);
+			return await jwt.verify(token, process.env.SECRET);
+		} catch (e) {
+			return null;
+			//throw new AuthenticationError("Meeting ended or Invalid Token.");
+		}
+	}
 };
 
 const server = new ApolloServer({
-  typeDefs: schema,
-  resolvers,
-  context: async ({ req, connection }) => {
-    if (connection) {
-      return {
-        models,
-      };
-    }
+	typeDefs: schema,
+	resolvers,
+	context: async ({ req, connection }) => {
+		if (connection) {
+			return {
+				models,
+			};
+		}
 
-    if (req) {
-      const me = await getMe(req);
-      const participantMe = await getParticipantMe(req);
-      return {
-        models,
-        me,
-        participantMe,
-        secret: process.env.SECRET,
-      };
-    }
-  },
+		if (req) {
+			const me = await getMe(req);
+			const participantMe = await getParticipantMe(req);
+			return {
+				models,
+				me,
+				participantMe,
+				secret: process.env.SECRET,
+			};
+		}
+	},
 });
 
 server.applyMiddleware({
-  app,
-  path: "/graphql",
-  bodyParserConfig: {
-    // needs changing
-    limit: "10mb",
-  },
+	app,
+	path: "/graphql",
+	bodyParserConfig: {
+		// needs changing
+		limit: "10mb",
+	},
 });
 
 const httpServer = http.createServer(app);
@@ -77,7 +78,7 @@ const isTest = true;
 
 // remove alter
 sequelize.sync({ alter: isTest }).then(async () => {
-  httpServer.listen({ port: 8000 }, () => {
-    console.log("Apollo Server on http://localhost:8000/graphql");
-  });
+	httpServer.listen({ port: 8000 }, () => {
+		console.log("Apollo Server on http://localhost:8000/graphql");
+	});
 });
