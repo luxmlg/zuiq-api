b930d5b915677609e32e25ca3bb54e181acaf314
Vordty <tokomostwanted@gmail.com>
2021-03-23 00:10:35 +0400

    Add ValidateMeetingResponse type to meeting schema

--- schema/meeting.js
+++ schema/meeting.js
@@ -2,28 +2,30 @@ import { gql } from "apollo-server-express";
 
 // meetings should be fetched using user id
 export default gql`
-  extend type Query {
-    meetings: [Meeting]!
-    meeting(id: ID!): Meeting!
-  }
+	extend type Query {
+		meetings: [Meeting]!
+		meeting(id: ID!): Meeting!
+		validateMeeting(meetingToken: String, participantToken: String): ValidateMeetingResponse!
+	}
 
-  extend type Mutation {
-    createMeeting(
-      name: String!
-      quizId: ID!
-      startTime: Date!
-      endTime: Date!
-    ): Meeting!
-    deleteMeeting(id: ID!): Boolean!
-  }
+	extend type Mutation {
+		createMeeting(name: String!, quizId: ID!, startTime: Date!, endTime: Date!): Meeting!
+		deleteMeeting(id: ID!): Boolean!
+	}
 
-  type Meeting {
-    id: ID!
-    quiz: Quiz!
-    name: String!
-    token: String!
-    participants: [Participant]!
-    startTime: Date!
-    endTime: Date!
-  }
+	type ValidateMeetingResponse {
+		success: Boolean
+		message: String
+		action: String
+	}
+
+	type Meeting {
+		id: ID!
+		quiz: Quiz!
+		name: String!
+		token: String!
+		participants: [Participant]!
+		startTime: Date!
+		endTime: Date!
+	}
 `;
