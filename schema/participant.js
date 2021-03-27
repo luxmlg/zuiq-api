def27f8cf61dfc7860b1574574cb58967570424d
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-03-27 17:38:36 +0400

    Add email to participant type in gql schema

--- schema/participant.js
+++ schema/participant.js
@@ -2,23 +2,24 @@ import { gql } from "apollo-server-express";
 
 // needs query participantMeeting: Meeting!
 export default gql`
-  extend type Query {
-    participants(meetingId: ID!): [Participant]!
-    participant(id: ID!): Participant!
-    getParticipantUsingToken(token: String!): Participant!
-  }
+	extend type Query {
+		participants(meetingId: ID!): [Participant]!
+		participant(id: ID!): Participant!
+		getParticipantUsingToken(token: String!): Participant!
+	}
 
-  extend type Mutation {
-    createParticipant(name: String!, token: String!): Token!
-    updateAnswers(answers: String!): Participant
-    completeMeeting: Participant
-  }
+	extend type Mutation {
+		createParticipant(name: String!, token: String!): Token!
+		updateAnswers(answers: String!): Participant
+		completeMeeting: Participant
+	}
 
-  type Participant {
-    id: ID!
-    meeting: Meeting
-    name: String!
-    hasCompleted: Boolean!
-    answers: String
-  }
+	type Participant {
+		id: ID!
+		meeting: Meeting
+		name: String!
+		email: String!
+		hasCompleted: Boolean!
+		answers: String
+	}
 `;
